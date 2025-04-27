901f05b7e4b48990c5ff843a1bb62a73460c97a1
jayzalani <151207678+jayzalani@users.noreply.github.com>
2025-04-27 10:39:48 +0530

    dep

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-import CustomCursor from "./components/CustomCursor";
+import CustomCursor from "../components/CustomCursor";
 
 
 const  jetbrains= JetBrains_Mono({
